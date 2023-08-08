bd20b0db034ebe00379979f58fe981de4c10f663
lihyel <0907leeharry@gmail.com>
2023-08-08 09:26:59 +0000

    first commit

---
+++ Solution_Note/is_evenly_divisible.py
@@ -0,0 +1,15 @@
+def is_evenly_divisible(number):
+    # 여기에 코드를 작성하세요
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+    
+    
+
+# 테스트 코드
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
+print(is_evenly_divisible(218))
+print(is_evenly_divisible(317))
