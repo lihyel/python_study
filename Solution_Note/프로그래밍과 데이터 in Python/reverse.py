7d31ace6cede9cc3adbf03a5db3747aeec3bca73
lihyel <0907leeharry@gmail.com>
2023-08-18 08:34:24 +0000

    230818 reverse

---
+++ Solution_Note/프로그래밍과 데이터 in Python/reverse.py
@@ -0,0 +1,18 @@
+# reverse() 쓰지 않고, list 뒤집어서 출력하기
+
+
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+#numbers.reverse() 
+
+# 리스트 뒤집기
+for left in range(len(numbers) // 2):
+    # 인덱스 left와 대칭인 인덱스 right 계산
+    right = len(numbers) - left - 1
+
+    # 위치 바꾸기
+    temp = numbers[left]
+    numbers[left] = numbers[right]
+    numbers[right] = temp
+
+print("뒤집어진 리스트: " + str(numbers))
